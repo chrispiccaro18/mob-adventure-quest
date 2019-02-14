f570b79fd96ee4dffb8a677cd76a5d0daedc3854
heather <unphurlng@gmail.com>
2019-02-14 13:04:05 -0800

    Added dragon quests

--- src/quests.js
+++ src/quests.js
@@ -26,10 +26,38 @@ const fire = {
     ]
 };
 
-const flood = {};
+const dragon = {
+    id: 'dragon',
+    title: 'dragon keep',
+    imgSrc: './assets/villagefire.jpg',
+    audioSrc: '../../assets/fire.mp3',
+    questDescription: 'A dragon is attacking the village of ___. You successfully infiltrate her lair and see a mountain of gold, jewels and treasure.',
+    choices: [
+        {
+            description: 'Negotiate with the dragon by giving them a sheep.',
+            hpChange: 15,
+            goldChange: 50,
+            result: 'The dragon is amused by your sheep, humors you with some gold. You gain 15 health points and 50 gold'
+        },
+        {
+            description: 'Brandish your sword and yell "hazzuhhhh!"',
+            hpChange: -45,
+            goldChange: 500,
+            result: 'You defeated the dragon, but suffered significant losses.'
+        },
+        {
+            description: 'Set a dragon trap.',
+            hpChange: -10,
+            goldChange: 100,
+            result: 'You grabbed what you could, while the dragon was entangled.'
+        }
+    ]
+};
+
+
 const blood = {};
 
-const quests = [fire, flood, blood];
+const quests = [fire, dragon, blood];
 
 
 
